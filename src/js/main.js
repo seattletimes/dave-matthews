8246e628d685005f8de041b68502aee74fa4a333
Miranda Hardy <mhardy@seattletimes.com>
2019-11-27 14:23:41 -0800

    buttons check and un check

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,32 @@ var paywall = require("./lib/paywall");
 setTimeout(() => paywall(12345678), 5000);
 
 require("component-responsive-frame/child");
+
+var albumButtons = document.querySelectorAll(".albumButtons .albumButton");
+
+
+//button check and uncheck
+function albumButton(){
+    var classes = this.classList.value;
+
+    if(classes.indexOf("checked") == -1){
+        var checked = document.querySelectorAll(".checked");
+        if(checked.length>0){
+            for(var x = 0; x < checked.length; x++){
+                checked[x].classList.remove("checked");
+            }
+        }
+        this.classList.add("checked");
+    }
+    else{
+        this.classList.remove("checked");
+    }
+}
+
+function albumButtonListener(){
+    for(var x = 0; x <albumButtons.length; x++){
+        albumButtons[x].addEventListener("click", albumButton);
+    }
+}
+
+albumButtonListener();
