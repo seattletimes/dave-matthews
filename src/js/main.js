7b4b7461ae33e1ab6d5a9409a2f77a6e96a32bda
Miranda Hardy <mhardy@seattletimes.com>
2019-12-02 12:14:40 -0800

    Dropdown albums, submit button with verification

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,11 @@ setTimeout(() => paywall(12345678), 5000);
 require("component-responsive-frame/child");
 
 var albumButtons = document.querySelectorAll(".albumButtons .albumButton");
+var select = document.querySelector("select");
+var submitButton = document.querySelector(".submit");
+var verify = document.querySelector(".verify");
+
+
 //var colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "DarkSlateBlue", "OrangeRed"];
 
 //button check and uncheck
@@ -17,8 +22,7 @@ function albumButton(){
                 checked[x].classList.remove("checked");
             }
         }
-        this.classList.add("checked");
-        
+        this.classList.add("checked");  
     }
     else{
         this.classList.remove("checked");
@@ -32,3 +36,22 @@ function albumButtonListener(){
 }
 
 albumButtonListener();
+
+function voteDropdown(){
+    // console.log(select.value);
+    verify.innerText = "";
+}
+select.addEventListener("change", voteDropdown);
+
+function submit(){
+    var vote = select.value;
+    if(vote == "default"){
+        verify.innerText = "Please select an album";
+    }
+    else{
+
+    }
+}
+
+submitButton.addEventListener("click", submit);
+
