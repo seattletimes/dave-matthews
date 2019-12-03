162d9a62c8e55920edc90b4e02138fe34adb7729
Miranda Hardy <mhardy@seattletimes.com>
2019-12-03 14:27:38 -0800

    can update the google sheet when submit a vote

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,9 @@ setTimeout(() => paywall(12345678), 5000);
 
 require("component-responsive-frame/child");
 var discog = require("../../data/discog.sheet.json");
+var votes = require("../../data/vote.sheet.json");
 
 var albumButtons = document.querySelectorAll(".albumButtons .albumButton");
-var select = document.querySelector("select");
-var submitButton = document.querySelector(".submit");
-var verify = document.querySelector(".verify");
 var stat = document.querySelector(".stat");
 
 var albumDetails = document.querySelector(".albumDetails")
@@ -17,6 +15,13 @@ var billboard = document.querySelector(".billboard");
 var grammy = document.querySelector(".grammy");
 var fact = document.querySelector(".fact");
 
+var select = document.querySelector("select");
+var submitButton = document.querySelector(".submit");
+var verify = document.querySelector(".verify");
+var scriptURL = 'https://script.google.com/macros/s/AKfycbyxFKs5OM9Mhr8XtVYSWVw_jIFILBjCWN2ywP6nBzPIjBKI4HA/exec'
+var form = document.forms['submit-to-google-sheet'];
+
+var voteTotals;
 
 //button check and uncheck
 function albumButton(){
@@ -63,37 +68,63 @@ function albumButtonListener(){
 
 albumButtonListener();
 
-function voteDropdown(){
-    // console.log(select.value);
-    verify.innerText = "";
+//reimplement this code so that a cookie drops and so we confirm they are voting!
+// function voteDropdown(){
+//     // console.log(select.value);
+//     verify.innerText = "";
+// }
+// select.addEventListener("change", voteDropdown);
+
+
+// function getCookie(name) {
+//     var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+//     return v ? v[2] : null;
+// }
+
+// function submit(){
+//     var vote = select.value;
+
+//     if(getCookie("vote")){
+//         verify.innerText = "Looks like you've already voted, come back tomorrow to vote again";
+//     }
+//     else if(vote == "default"){
+//         verify.innerText = "Please select an album";
+//     }
+//     else{
+//         var albumName = select.options[select.selectedIndex].text;
+//         var statNum = 50;
+//         var d = new Date();
+//         d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+
+//         document.cookie = "vote="+vote + "; expires=" + d.toGMTString() + ";";
+//         stat.innerText = statNum + "% of voters agree that " + albumName + " is Dave Matthews Band's Best album"
+//     }
+// }
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  });
+
+function tallyVotes(){
+    if(votes.length){  
+        voteTotals = {};
+        var length = votes.length;
+        var add;
+
+        for(var x = 0; x < length; x++){
+            add = votes[x].album;
+            if(voteTotals[add] >= 0){
+                voteTotals[add] +=1;
+            } else{
+                voteTotals[add] = 1;
+            }
+        }
+    }  
 }
-select.addEventListener("change", voteDropdown);
 
+tallyVotes();
 
-function getCookie(name) {
-    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return v ? v[2] : null;
-}
-
-function submit(){
-    var vote = select.value;
-
-    if(getCookie("vote")){
-        verify.innerText = "Looks like you've already voted, come back tomorrow to vote again";
-    }
-    else if(vote == "default"){
-        verify.innerText = "Please select an album";
-    }
-    else{
-        var albumName = select.options[select.selectedIndex].text;
-        var statNum = 50;
-        var d = new Date();
-        d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
-
-        document.cookie = "vote="+vote + "; expires=" + d.toGMTString() + ";";
-        stat.innerText = statNum + "% of voters agree that " + albumName + " is Dave Matthews Band's Best album"
-    }
-}
-
-submitButton.addEventListener("click", submit);
 
