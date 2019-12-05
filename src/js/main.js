c624122ab73438ef4069042e093570bd9c27ad52
Miranda Hardy <mhardy@seattletimes.com>
2019-12-05 13:42:59 -0800

    added cookies to submit, need to figure out bug

--- src/js/main.js
+++ src/js/main.js
@@ -19,10 +19,12 @@ var grammy = document.querySelector(".grammy");
 var fact = document.querySelector(".fact");
 
 var select = document.querySelector("select");
+var viewResults = document.querySelector(".viewResults");
 var submitButton = document.querySelector(".submit");
 var verify = document.querySelector(".verify");
 var scriptURL = 'https://script.google.com/macros/s/AKfycbyxFKs5OM9Mhr8XtVYSWVw_jIFILBjCWN2ywP6nBzPIjBKI4HA/exec'
 var form = document.forms['submit-to-google-sheet'];
+var svgContainer = document.querySelector(".svg-container");
 
 //button check and uncheck
 function albumButton(){
@@ -67,58 +69,56 @@ function albumButtonListener(){
 
 albumButtonListener();
 
-//reimplement this code so that a cookie drops and so we confirm they are voting!
-// function voteDropdown(){
-//     // console.log(select.value);
-//     verify.innerText = "";
-// }
-// select.addEventListener("change", voteDropdown);
+
+//poll section
+
+function getCookie(name) {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
+}
 
 
-// function getCookie(name) {
-//     var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-//     return v ? v[2] : null;
-// }
 
-// function submit(){
-//     var vote = select.value;
+function submitHandler(e){
+    var vote = select.value;
 
-//     if(getCookie("vote")){
-//         verify.innerText = "Looks like you've already voted, come back tomorrow to vote again";
-//     }
-//     else if(vote == "default"){
-//         verify.innerText = "Please select an album";
-//     }
-//     else{
-//         var albumName = select.options[select.selectedIndex].text;
-//         var statNum = 50;
-//         var d = new Date();
-//         d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+    if(vote == "default"){
+        verify.innerText = "Please select an album";
+    }
+    else if(getCookie("vote")){
+        verify.innerText = "Looks like you've already voted, come back tomorrow to vote again";
+        init();
+        svgContainer.style.display = "inline-block";
+    }
+    else{
+        verify.innerText = "";
+        var d = new Date();
+        d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
 
-//         document.cookie = "vote="+vote + "; expires=" + d.toGMTString() + ";";
-//         stat.innerText = statNum + "% of voters agree that " + albumName + " is Dave Matthews Band's Best album"
-//     }
-// }
+        document.cookie = "vote="+vote + "; expires=" + d.toGMTString() + ";";
+        
+        e.preventDefault();
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+          .then(response => console.log('Success!', response))
+          .catch(error => console.error('Error!', error.message));
+        setTimeout(function() { init(); }, 1000);
+        svgContainer.style.display = "inline-block";
+    }
+}
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message));
-    setTimeout(function() { init(); }, 2000);
-  });
+
 
 function tally(voteData){
     if(voteData.length > 0){  
-        var totals =      [   { name: "underTheTableAndDreaming", count: 0},
-        { name: "crash", count: 0},
-        { name: "beforeTheseCrowdedStreets", count: 0},
-        { name: "everyday", count: 0 },
-        { name: "bustedStuff", count: 0},
-        { name: "standUp", count: 0},
-        { name: "bigWhiskey", count: 0}, 
-        { name: "awayFromTheWorld", count: 0 },
-        { name: "comeTomorrow", count: 0} ];
+        var totals =      [   { name: "underTheTableAndDreaming", label: "Under the Table and Dreaming", count: 0},
+        { name: "crash", label: "Crash", count: 0},
+        { name: "beforeTheseCrowdedStreets", label: "Before These Crowded Streets", count: 0},
+        { name: "everyday", label: "Everyday", count: 0 },
+        { name: "bustedStuff", label: "Busted Stuff", count: 0},
+        { name: "standUp", label: "Stand Up", count: 0},
+        { name: "bigWhiskey", label: "Big Whiskey", count: 0}, 
+        { name: "awayFromTheWorld", label: "Away From The World", count: 0 },
+        { name: "comeTomorrow", label: "Come Tomorrow", count: 0} ];
         var length = voteData.length;
         var add = "";
 
@@ -126,19 +126,22 @@ function tally(voteData){
             add = voteData[x].album;
                 for(var y = 0; y < totals.length; y++){
                     if(totals[y].name == add){
-                        console.log(totals[y]);
                         totals[y].count += 1;
-                    }
                 }
             }
         }
-        return totals;
-    }  
+        for(var x = 0; x < totals.length; x++){
+            var percent = totals[x].count/voteData.length;
+            totals[x].count = percent;
+         }
+    }
+    return totals;
+}  
 
 
 
 var publicSpreadSheetUrl = "https://docs.google.com/spreadsheets/d/1odzOwj1yo6HAwE4pMyvH4OkG2CR6cNcnXouPSPoLHe8/edit?usp=sharing"
-//var voteData;
+
 function init() {
     Tabletop.init( { key: publicSpreadSheetUrl,
                      callback: showInfo,
@@ -147,14 +150,54 @@ function init() {
 
 function showInfo(data, tabletop) {
     var allVotes = tabletop.sheets("vote").elements;
-
     var totals = tally(allVotes);
-    console.log(data);
+    
+    totals.sort(function(a, b){
+        return d3.ascending(a.count, b.count);
+    });
+      
+    var svg = d3.select("svg");
+    var margin = {top: 10, right: 20, bottom: 100, left: 10};
+    var width = +svg.attr("width") - margin.left - margin.right ;
+    var height = +svg.attr("height") - margin.top - margin.bottom;
 
-//build with d3 here
 
+    var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05);
+    var y = d3.scaleLinear().rangeRound([height, 0]);
 
+    var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    x.domain(totals.map(function(d) { return d.label; }));
+    y.domain([0, d3.max(totals, function(d) { return d.count; })]);
+
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    var bar = g.selectAll(".bar")
+      	.data(totals)
+        .enter().append("rect")
+        .attr("x", function(d) { return x(d.label); })
+        .attr("y", function(d) { return y(d.count); })
+        .attr("width", x.bandwidth())
+        .attr("height", function(d) { return height - y(d.count); });
+
+    g.selectAll("text")
+        .attr("transform", "rotate(30)")
+        .style("font-size", "12px")
+        .style("text-anchor", "start");
   }
 
-init();
+
+form.addEventListener('submit', function(a){
+    submitHandler(a);
+});
+
+viewResults.addEventListener('click', function(a){
+    verify.innerText = "";
+    init();
+    svgContainer.style.display = "inline-block";
+});
 
